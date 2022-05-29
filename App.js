f2cf7b8c74955cd5b303d425d3211553da832417
pygamer80 <pygamer80@gmail.com>
2022-05-29 15:27:36 -0300

    mapasERotas

--- App.js
+++ App.js
@@ -1,67 +1,15 @@
 import React from "react"
-import { StyleSheet, TouchableOpacity, View, SafeAreaView, Text, Alert,Image } from "react-native"
-
+import {View, Text} from "react-native"
+import { css } from "./assets/img/css/css"
 
 
 export default function App(){ 
-  const signin = ()=>{
-    alert('Cliquei')
-  }
 
 return(
-  <SafeAreaView style={style.container}>
-    <View>
-  <TouchableOpacity 
-  style={style.buttonFacebookStyle}
-  activeOpacity={0.5}>
-      <Image 
-      source={require('./img/face.png')}
-      style={style.buttonImageIconStyle} />
-      <Text style={style.title}>Login usando facebook</Text>
-  </TouchableOpacity>
-  </View>
-  </SafeAreaView>
+  <View style ={css.container}>
+    <View style={css.map}></View>
+
+    <View style={css.search}></View>
+</View>
 )
 }
-const style = StyleSheet.create({
-  container:{
-    flex:1,
-    justifyContent:'center',
-    marginHorizontal:16,
-},
-  
-  buttonFacebookStyle:{
-    flexDirection:'row',
-    alignItems:'center',
-    backgroundColor:'#00539F',
-    borderWidth:0.5,
-    borderColor:'#fff',
-    height:40,
-    borderRadius:5,
-    margin:5,
-},
-buttonImageIconStyle:{
-  padding:10,
-  margin:0,
-  height:'163%',
-  width:'30%',
-  resizeMode:'stretch',
-  alignContent:'center'
-},
-buttonIconSeparatorStyle:{
-  backgroundColor:'#fff',
-  width:1,
-  height:40
-},
-buttonTextStyle:{
-  color:'#fff',
-  marginBottom:4,
-  marginLeft:10
-},
-title:{
-  color:'#fff',
-  fontSize:16, 
-  fontWeight:'bold',
-  marginLeft:35
-}
-})
---
+++ assets/img/css/css.js
@@ -0,0 +1,19 @@
+import { StyleSheet } from "react-native"
+
+const css = StyleSheet.create({
+    container:{
+      flex:1,
+      justifyContent:'center',
+      backgroundColor:'#fff'
+  },
+  map:{
+    height:'60%',
+    backgroundColor:"black",
+  },
+  
+  search:{
+    height:'40%',
+    backgroundColor:"grey"
+  }
+})
+export {css}
